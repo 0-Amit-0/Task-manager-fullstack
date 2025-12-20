5b2c8b27643a5c2b68bc9fc2647daddaf94261af
0-Amit-0 <121421318+0-Amit-0@users.noreply.github.com>
2025-12-20 20:34:09 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = 5000;
+
+app.get('/', (req, res) => {
+    res.send("Backend is running successfully!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+
